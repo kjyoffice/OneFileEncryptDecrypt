69fb36e283ae307dd724a1a2a1faf41d8089a376
KimJaeYoung <kjyoffice@gmail.com>
2026-01-09 22:27:06 +0900

    Start. Sample Work

---
+++ OneFileEncryptDecrypt/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OneFileEncryptDecrypt
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var args = Environment.GetCommandLineArgs();
+            var configX = new ConfigurationBuilder().AddJsonFile("appsettings.json", true).AddCommandLine(args).Build();
+            var xx = configX.Get<XAppSettings_Json.AppSettingsMain_Json>();
+
+            Console.Out.WriteLine(xx?.WorkID);
+            Console.Out.WriteLine(xx?.Say);
+
+            Console.Out.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ OneFileEncryptDecrypt/XAppSettings_Json/AppSettingsMain_Json.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OneFileEncryptDecrypt.XAppSettings_Json
+{
+    public class AppSettingsMain_Json
+    {
+        //[Microsoft.Extensions.Configuration.ConfigurationKeyName("workID")]
+        public string? WorkID { get; set; }
+        public string? Say { get; set; }
+    }
+}
