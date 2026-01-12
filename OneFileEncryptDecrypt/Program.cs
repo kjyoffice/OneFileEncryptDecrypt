fc8ed259c56deca8aab0e5339ebf5b5446dacfe4
KimJaeYoung <kjyoffice@gmail.com>
2026-01-13 06:34:07 +0900

    AesGcm 암호화시 청크 의견 추가

--- OneFileEncryptDecrypt/Program.cs
+++ OneFileEncryptDecrypt/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.CommandLine;
+using System.CommandLine;
 using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
@@ -119,6 +119,8 @@ namespace OneFileEncryptDecrypt
             Console.WriteLine(Encoding.UTF8.GetString(decryptX));
             */
             // 돌긴 하는데.. GPT왈, 고용량 데이터(Ex 1GB) 암호화는 청크로 잘라서 해란다...
+            // Nonce도 파일별 하나 받아서 청크별 인덱스 붙여서 중복 nonce 없게 하라고 하고
+            // 섞이는거 대비 aad에 정보로 청크별 순서도 같이 기록해라네 
             // 복호화 역시 청크로 잘린거 복호화 해서 이어 붙여야 한다는건데...
             // 이건 나중에 차기 버젼에 하기로 하자 ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ ㅠㅠ
             // 아니면 바운스캐슬로 해야할듯
