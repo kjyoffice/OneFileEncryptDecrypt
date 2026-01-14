db58be6e6b3eb076e34342e57affc43a1372a92a
KimJaeYoung <kjyoffice@gmail.com>
2026-01-14 21:17:52 +0900

    Object를 JSON으로 바꾸는거 추가

---
+++ OneFileEncryptDecrypt/XWork/JsonWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OneFileEncryptDecrypt.XWork
+{
+    public class JsonWork
+    {
+        public static string ToJsonText(object data)
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(
+                data,
+                new Newtonsoft.Json.JsonSerializerSettings() 
+                { 
+                    Formatting = Newtonsoft.Json.Formatting.Indented
+                }
+            );
+        }
+    }
+}
