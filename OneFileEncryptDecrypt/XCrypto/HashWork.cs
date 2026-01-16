faed3bd3151b61c07d388ddfd02f604df143819e
KimJaeYoung <kjyoffice@gmail.com>
2026-01-16 20:47:47 +0900

    HMAC 추가하고 기존의 일반 HASH는 비활성화

--- OneFileEncryptDecrypt/XCrypto/HashWork.cs
+++ OneFileEncryptDecrypt/XCrypto/HashWork.cs
@@ -10,13 +10,51 @@ namespace OneFileEncryptDecrypt.XCrypto
 {
     public class HashWork
     {
-        public static byte[] CreateSHA512(byte[] source, string title, XModel.ProgressViewer? pv)
+        // Keep!
+        private static byte[] CreateSHA512(byte[] source, string title, XModel.ProgressViewer? pv)
         {
             var chunkSize = XValue.ProcessValue.BufferChunkSize;
             var offset = 0;
             var hashList = new List<byte>();    
 
             using (var hash = SHA512.Create())
+            {
+                pv?.Start(title, source.Length);
+
+                while (offset < source.Length)
+                {
+                    var readBytes = Math.Min(chunkSize, (source.Length - offset));
+
+                    // chunk 단위 해시
+                    hash.TransformBlock(source, offset, readBytes, null, 0);
+
+                    offset += readBytes;
+
+                    // 진행 표시
+                    pv?.AddProgress(readBytes);
+                    pv?.ProgressDisplay();
+                }
+
+                hash.TransformFinalBlock(Array.Empty<byte>(), 0, 0);
+
+                hashList.AddRange(hash.Hash!);
+
+                hash.Clear();
+                pv?.Done();
+            }
+
+            var result = hashList.ToArray();
+
+            return result;
+        }
+
+        public static byte[] CreateSHA512HMAC(byte[] source, byte[] key, string title, XModel.ProgressViewer? pv)
+        {
+            var chunkSize = XValue.ProcessValue.BufferChunkSize;
+            var offset = 0;
+            var hashList = new List<byte>();
+
+            using (var hash = new HMACSHA512(key))
             {
                 pv?.Start(title, source.Length);
 
